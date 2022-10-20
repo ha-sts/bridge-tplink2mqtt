95029ba9ce8db4bdd9e7ed177a26700d08b2f186
Daniel Williams <dwilliams@port8080.net>
2022-10-20 13:34:20 -0600

    Starting the project.

---
+++ run.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+### IMPORTS ###
+
+### GLOBALS ###
+
+### FUNCTIONS ###
+
+### CLASSES ###
+
+### MAIN ###
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
